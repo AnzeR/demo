ccb6caf41de1502299d502f4137663fc599b91cb
Anže Robida s.p <anze.robida@gmail.com>
2022-03-19 11:50:54 +0100

    Edit

--- Demo/Pages/InvoiceEdit.cshtml.cs
+++ Demo/Pages/InvoiceEdit.cshtml.cs
@@ -5,7 +5,7 @@ using System.Data.SqlClient;
 
 namespace Demo.Pages
 {
-    public class InvoiceModel : PageModel
+    public class InvoiceEditModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
         private readonly IConfiguration _configuration;
@@ -15,7 +15,7 @@ namespace Demo.Pages
         [FromQuery(Name = "id")]
         public int? InvoiceId { get; set; }
 
-        public InvoiceModel(ILogger<IndexModel> logger, IConfiguration configuration)
+        public InvoiceEditModel(ILogger<IndexModel> logger, IConfiguration configuration)
         {
             _logger = logger;
             _configuration = configuration;
@@ -27,7 +27,8 @@ namespace Demo.Pages
                 con.Open();
                 IDatabase db = new Database(con);
 
-                if (InvoiceId.HasValue) {
+                if (InvoiceId.HasValue)
+                {
                     invoice = db.Fetch<InvoiceDTO>("select i.id, i.issue_date, c.name as client_name from invoice i inner join client c on c.id = i.id_client where i.id = @id", new { id = InvoiceId }).First();
 
                     invoice.Lines = db.Fetch<InvoiceLineDTO>(@"select 
@@ -45,9 +46,37 @@ namespace Demo.Pages
                     join client c on c.id = i.id_client
                     inner
                     join article a on a.id = il.id_article
-                    where i.id = @id", new { id = InvoiceId});
+                    where i.id = @id", new { id = InvoiceId });
+                } 
+                else
+                {
+                    invoice = new InvoiceDTO();
+                    invoice.Lines = new List<InvoiceLineDTO>();
                 }
             }
+        }
+
+        public void OnPost()
+        {
+            using (SqlConnection con = new SqlConnection(_configuration.GetConnectionString("main")))
+            {
+                con.Open();
+                IDatabase db = new Database(con);
+
+                var IssueDate = DateTime.Parse(Request.Form["IssueDate"]);
+
+                if (InvoiceId.HasValue)
+                {
+                    // Update
+                    var affected = db.Update("update invoice set issue_date = @issueDate where id = @id", new { id = InvoiceId, issueDate = IssueDate });
+                }
+                else
+                {
+                    // Insert
+                }
+            }
+
+            // RedirectPermanent("/invoiceedit?id=10");
         }
     }
 }
