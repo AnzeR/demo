ebd351a9f28f345c5ab776dd986fb304eb6f076f
Anže Robida s.p <anze.robida@gmail.com>
2022-03-19 08:27:33 +0100

    Initial project with DB connection

---
+++ Demo/Pages/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using NPoco;
+using System.Data.SqlClient;
+
+namespace Demo.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _configuration;
+
+        public string sqlTime;
+
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        public void OnGet()
+        {
+            using (SqlConnection con = new SqlConnection(_configuration.GetConnectionString("main")))
+            {
+                con.Open();
+                IDatabase db = new Database(con);
+                sqlTime = db.Fetch<string>("select getdate()").First();
+            }
+        }
+    }
+}
